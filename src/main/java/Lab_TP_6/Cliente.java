449ac408ede452ac3c276a7642c226bcadc485a9
Ikaros <emmanuel1631@gmail.com>
2022-04-13 00:44:50 -0300

    First Upload

---
+++ src/main/java/Lab_TP_6/Cliente.java
@@ -0,0 +1,18 @@
+package Lab_TP_6;
+
+public class Cliente {
+
+    private long dni;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String ciudad;
+
+    private String direccion;
+
+    public Cliente(long p_dni, String p_nombre, String p_apellido, String p_ciudad, String p_direccion) {
+        this.dni=
+    }
+}
